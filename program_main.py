f268c836a0e377794364b8e85ac143abcb7f54a9
yashsawkar <sawkaryash257@gmail.com>
2022-10-17 20:48:24 +0530

    reading the csv file

--- program_main.py
+++ program_main.py
@@ -1,9 +1,9 @@
 
 import random as rd
+import pandas as pd
+import matplotlib as plt
+
 import time as tm
-
-
-
 
 def pre_info():
      syn = "\n\nThis is program under development \n" + tm.ctime() + "\nThe data used can be excrated in vehicles.csv FILE\n\n"
@@ -12,3 +12,6 @@ def pre_info():
           tm.sleep(0.02)
      
 pre_info()
+
+data_read = pd.read_csv("vehicles.csv")
+print(data_read)
