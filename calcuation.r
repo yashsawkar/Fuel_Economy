d9ac62a7faf2f74d8db62cebd6747e7e189a2588
yashsawkar <sawkaryash257@gmail.com>
2022-10-17 14:23:40 +0530

    intialised the repostory

---
+++ calcuation.r
@@ -0,0 +1,5 @@
+
+
+"This is the R lagnuage file "
+
+" This file will b used for calcuation purposes"
---
+++ program_main.py
@@ -0,0 +1,14 @@
+
+import random as rd
+import time as tm
+
+
+
+
+def pre_info():
+     syn = "\n\nThis is program under development \n" + tm.ctime() + "\nThe data used can be excrated in vehicles.csv FILE\n\n"
+     for i in syn:
+          print(i,end="")
+          tm.sleep(0.02)
+     
+pre_info()
