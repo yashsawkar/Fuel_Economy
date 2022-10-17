2baf6a04b694dec497e24525b931ac77cae8d5c5
yashsawkar <sawkaryash257@gmail.com>
2022-10-18 00:45:33 +0530

    Cleaning the data set

--- main_pro.py
+++ main_pro.py
@@ -13,10 +13,36 @@ def pre_info():
      
 pre_info()
 
+# Holds whole data 
 data = pd.read_csv("vehicles.csv")
-print("This is the head data : \n")
-print(data.head(6))
-print("this is the tail data : \n")
-print(data.tail(6))
 
-print("harshit signed in")
+# Holds only the segements of different data holding
+data_segments = data.dropna() 
+
+# Filling the empty dat with random values ranging from (10-60)
+data["barrelsA08"].fillna(rd.randint(10,60), inplace = True)
+
+# Standard procedure
+# Removing the unwanted data segements
+data.pop("phevHwy")
+data.pop("phevCity")
+data.pop("phevComb")
+data.pop("startStop")
+
+# Considering only first 10 elements as preview 
+print(data.head(10))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
