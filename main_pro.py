3e360e386cdba4a8134d40b10b4c59fe23b613d5
yashsawkar <sawkaryash257@gmail.com>
2022-10-17 22:54:11 +0530

    created the main file

---
+++ main_pro.py
@@ -0,0 +1,20 @@
+
+
+import random as rd
+import pandas as pd
+
+import time as tm
+
+def pre_info():
+     syn = "\n\nThis is program under development \n" + tm.ctime() + "\nThe data used can be excrated in vehicles.csv FILE\n\n"
+     for i in syn:
+          print(i,end="")
+          tm.sleep(0.02)
+     
+pre_info()
+
+data = pd.read_csv("vehicles.csv")
+print("This is the head data : \n")
+print(data.head(3))
+print("this is the tail data : \n")
+print(data.tail(3))
