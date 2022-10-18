275a34b5dc68e74afe0bb228aea4c823b5517003
Aditya8404 <adityapunia4@gmail.com>
2022-10-18 21:36:01 +0530

    trial for cleaning data

--- main_pro.py
+++ main_pro.py
@@ -22,12 +22,25 @@ data_segments = data.dropna()
 # Filling the empty dat with random values ranging from (10-60)
 data["barrelsA08"].fillna(rd.randint(10,60), inplace = True)
 
+# Making list of all column names
+col = []
+col = list(data.columns)
+
 # Standard procedure
-# Removing the unwanted data segements
-data.pop("phevHwy")
-data.pop("phevCity")
-data.pop("phevComb")
-data.pop("startStop")
+# Removing the unwanted data segments
+i = 0
+var = col[0]
+size = len(col)
+for index, row in data.iterrows():
+     if row[var] == 0:
+          print(row[var])
+     #if i < size - 1:
+          #i = i + 1
+          #var = col[index+1]     
+
+# Printing all column 
+print(data_segments.head())
+
 
 # Considering only first 10 elements as preview 
 print(data.head(10))
