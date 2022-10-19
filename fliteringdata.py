1f9e85798324e47b3858a262eae732f7eb7e4259
yashsawkar <sawkaryash257@gmail.com>
2022-10-19 23:28:12 +0530

    Finally Cleaned....!!
    Created a new csv file and loaded all the required data segments..
    
    Please run this program in the PC to work with same dataset

--- fliteringdata.py
+++ fliteringdata.py
@@ -9,34 +9,82 @@ def pre_info():
      for i in syn:
           print(i,end="")
           tm.sleep(0.005)
+          
+def interation():
+     pass
      
 pre_info()
 
-## Execrate the requied data and place main_vec.csv
-collection = {
-               "Car_type" : [6,5,4],
-               "Mileage" : [14,16,18],
-               "Distance" : [40,5,76]
-               }
-
-data_collection = pd.DataFrame(collection)
-print(data_collection)
-
-data_collection.to_csv("main_vec.csv")
-
-print("\nThe csv file is holding the folloing data for testing purposes : \n")
-
-# print the new csv file
-data =pd.read_csv("main_vec.csv")
-print(data)
-print("\n\n")
-
-
-# printing the main file
+# printing the main ^BIG^ file
 big_data = pd.read_csv("vehicles.csv")
 print(big_data)
 print("This is all columns and they data type")
 print(big_data.info())
+
+#loading the big csv file into the data frame 
+datafram = pd.DataFrame(big_data)
+print(datafram)
+
+
+
+collection = {
+               "Car_type" : [],
+               "Fuel_type" : [],
+               "Drive" : [],
+               "Fuel_consumtion" : [],
+               "vehicle_brand" : [],
+               "Transmission" : [],
+               "cylinder" : [],
+               "cubic_capacity" : []
+               }
+
+for i in datafram["VClass"]:
+     print(i)
+     collection["Car_type"].append(i)
+     
+for i in datafram["fuelType"]:
+     print(i)
+     collection["Fuel_type"].append(i)
+     
+for i in datafram['drive']:
+     print(i)
+     collection["Drive"].append(i)
+     
+for i in datafram['barrels08']:
+     print(i)
+     collection["Fuel_consumtion"].append(i)
+     
+for i in datafram['make']:
+     print(i)
+     collection["vehicle_brand"].append(i)
+     
+for i in datafram['trany']:
+     print(i)
+     collection["Transmission"].append(i)
+     
+for i in datafram["cylinders"]:
+     print(i)
+     collection["cylinder"].append(i)
+     
+for i in datafram["displ"]:
+     print(i)
+     collection["cubic_capacity"].append(i)
+     
+
+dataframe_collection = pd.DataFrame(collection)
+print(dataframe_collection)
+
+# Now transfering this dataframe into a main_vehicles.csv file
+dataframe_collection.to_csv("main-data.csv")
+
+# printing the csv file to conform the placement of elements in the csv file
+read_data = pd.read_csv("main-data.csv")
+print(read_data.to_string())
+
+
+
+
+
 
 
 
